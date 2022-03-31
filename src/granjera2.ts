efcf57d8bdee67c78efbb87d8e5af63d5c926814
esteban36363223 <102192175+esteban36363223@users.noreply.github.com>
2022-03-31 17:20:17 -0300

    Initial commit

---
+++ src/granjera2.ts
@@ -0,0 +1,9 @@
+console.log("avanzar una parcela");
+console.log("llenar pozo");
+console.log("avanzar tres parcelas");
+console.log("llenar pozo");
+console.log("avanzar una parcela");
+console.log("aplanar pila");
+console.log("avanzar una parcela");
+console.log("aplanar pila");
+console.log("Tarea finalizada");
